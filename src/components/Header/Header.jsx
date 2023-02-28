a169bf2c9cf665a4ebdd889c9fa29f16d1cc3e70
wensow <andrewtumanov22@gmail.com>
2023-03-01 02:09:19 +0300

    change css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <div className={styles.wrapper}>
-      <h4>Pure Shop 👋</h4>
+      <h4>Pure Shop👋</h4>
       <p>Низкие цены, лучшее качество</p>
     </div>
   );
