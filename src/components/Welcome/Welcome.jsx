d0872806c2c7a02b869cbc3734298631dbff76d9
wensow <andrewtumanov22@gmail.com>
2023-03-01 01:50:32 +0300

    change css

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,6 +4,8 @@ const Welcome = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.category}>
+        <h2>Категории товаров</h2>
+
         <h3>Прочее: </h3>
         <a href="/">Twitter Blue</a>
         <h3>Аккаунты:</h3>
